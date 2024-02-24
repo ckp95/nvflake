7b3c4d1046864acc42c74d38f1413801a4c702ec
Cameron Pinnegar <cameron.pinnegar@gmail.com>
2024-02-24 18:33:11 +0000

    fix typo

--- packages/my-neovim.nix
+++ packages/my-neovim.nix
@@ -1,3 +1,3 @@
 { pkgs }:
-let customRc = import ../config { inherit pkgs; };
-in pkgs.wrapNeovim pkgs.neovim { configure = { inherit customRc; }; }
+let customRC = import ../config { inherit pkgs; };
+in pkgs.wrapNeovim pkgs.neovim { configure = { inherit customRC; }; }
