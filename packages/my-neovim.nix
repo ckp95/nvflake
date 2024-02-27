a128c74563f14a00b6584fee8f93d08b57d05373
Cameron Pinnegar <cameron.pinnegar@gmail.com>
2024-02-27 19:11:39 +0000

    add runtime deps

--- packages/my-neovim.nix
+++ packages/my-neovim.nix
@@ -2,10 +2,28 @@
 let
   customRC = import ../config { inherit pkgs; };
   plugins = import ../plugins.nix { inherit pkgs; };
-in
-  pkgs.wrapNeovim pkgs.neovim {
+  runtimeDeps = import ../runtimeDeps.nix { inherit pkgs; };
+  # https://ertt.ca/blog/2022/01-12-nix-symlinkJoin-nodePackages/
+  neovimRuntimeDependencies1 = pkgs.symlinkJoin {
+    name = "neovimRuntimeDependencies1";
+    paths = runtimeDeps.deps1;
+  };
+  neovimRuntimeDependencies2 = pkgs.symlinkJoin {
+    name = "neovimRuntimeDependencies2";
+    paths = runtimeDeps.deps2;
+  };
+  myNeovimUnwrapped = pkgs.wrapNeovim pkgs.neovim {
     configure = {
       inherit customRC;
       packages.all.start = plugins;
     };
+  };
+in
+  pkgs.writeShellApplication {
+    name = "nvim";
+    # these get added to the PATH environmental variable when neovim is run:
+    runtimeInputs = [ neovimRuntimeDependencies2 neovimRuntimeDependencies1 ];
+    text = ''
+      ${myNeovimUnwrapped}/bin/nvim "$@"
+    '';
   }
