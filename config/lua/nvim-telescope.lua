5a7617f416cfe4870ea396e7bf8e3402e02ec253
Cameron Pinnegar <cameron.pinnegar@gmail.com>
2024-02-27 21:35:35 +0000

    add telescope bindings

--- config/lua/nvim-telescope.lua
+++ config/lua/nvim-telescope.lua
@@ -1,15 +1,24 @@
-local opt = { noremap = true }
 local telescope = require("telescope")
 telescope.setup({})
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader><tab>",
-  ":lua require('telescope.builtin').find_files()<CR>",
-  opt
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>?",
-  ":lua require('telescope').extensions.recent_files.pick()<CR>",
-  opt
-)
+
+local nmap = function(lhs, rhs, desc)
+  return vim.keymap.set(
+    "n", lhs, rhs, { desc = desc }
+  )
+end
+
+nmap("<leader>ff",
+     require('telescope.builtin').find_files,
+     "[ff] Find files")
+nmap("<leader><space>",
+     require('telescope.builtin').buffers,
+     "[ ] Find existing buffers")
+nmap("<leader>fr",
+     require('telescope').extensions.recent_files.pick,
+     "[fr] Find recent files")
+nmap("<leader>fh",
+     require('telescope.builtin').help_tags,
+     "[fh] Find help")
+nmap("<leader>fk",
+     require('telescope.builtin').keymaps,
+     "[fk] Find keymaps")
