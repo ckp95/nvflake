077270aaf003f2f8188c0cbc64fc45d29572a415
Cameron Pinnegar <cameron.pinnegar@gmail.com>
2024-02-27 19:53:14 +0000

    fix typescript runtime dependencies bug

--- packages/my-neovim.nix
+++ packages/my-neovim.nix
@@ -3,14 +3,16 @@ let
   customRC = import ../config { inherit pkgs; };
   plugins = import ../plugins.nix { inherit pkgs; };
   runtimeDeps = import ../runtimeDeps.nix { inherit pkgs; };
-  # https://ertt.ca/blog/2022/01-12-nix-symlinkJoin-nodePackages/
-  neovimRuntimeDependencies1 = pkgs.symlinkJoin {
-    name = "neovimRuntimeDependencies1";
-    paths = runtimeDeps.deps1;
-  };
-  neovimRuntimeDependencies2 = pkgs.symlinkJoin {
-    name = "neovimRuntimeDependencies2";
-    paths = runtimeDeps.deps2;
+  neovimRuntimeDependencies = pkgs.symlinkJoin {
+    name = "neovimRuntimeDependencies";
+    paths = runtimeDeps;
+    # see: https://ertt.ca/blog/2022/01-12-nix-symlinkJoin-nodePackages/
+    postBuild = ''
+      for f in $out/lib/node_modules/.bin/*; do
+        path="$(readlink --canonicalize-missing "$f")"
+        ln -s "$path" "$out/bin/$(basename $f)"
+      done
+    '';
   };
   myNeovimUnwrapped = pkgs.wrapNeovim pkgs.neovim {
     configure = {
@@ -22,7 +24,7 @@ in
   pkgs.writeShellApplication {
     name = "nvim";
     # these get added to the PATH environmental variable when neovim is run:
-    runtimeInputs = [ neovimRuntimeDependencies2 neovimRuntimeDependencies1 ];
+    runtimeInputs = [ neovimRuntimeDependencies ];
     text = ''
       ${myNeovimUnwrapped}/bin/nvim "$@"
     '';
