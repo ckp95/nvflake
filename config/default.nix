89991c25089701e667f03d8cbe3b6c0957a5d753
Cameron Pinnegar <cameron.pinnegar@gmail.com>
2024-02-27 20:02:09 +0000

    add ultisnip stuff

--- config/default.nix
+++ config/default.nix
@@ -28,6 +28,7 @@ let
       + " ${file}") files);
 
   vim = scripts2ConfigFiles "vim";
+  vimnix = scripts2ConfigFiles "vimnix";
   lua = scripts2ConfigFiles "lua";
   luanix = nixFiles2ConfigFiles "luanix";
 
