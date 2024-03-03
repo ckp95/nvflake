3a7c78a2886909f492e88968e34e111080c4300c
Cameron Pinnegar <cameron.pinnegar@gmail.com>
2024-03-03 21:06:33 +0000

    add incremental selection

--- config/lua/nvim-plugins.lua
+++ config/lua/nvim-plugins.lua
@@ -19,9 +19,14 @@ require("nvim-treesitter.configs").setup{
   -- indent = {
   --   enable = true
   -- },
-  -- incremental_selection = {
-  --   enable = true
-  -- },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "v",
+      node_incremental = "v",
+      node_decremental = "V",
+    }
+  },
 }
 --
 -- completions
